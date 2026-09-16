14603993e462db2ef24ea7945152bba5bc7f931e
ryofukazawa324-ai <ryofukazawa324@gmail.com>
2026-09-16 21:59:51 +0900

    Refresh home screen build marker

--- freshness.js
+++ freshness.js
@@ -1,5 +1,5 @@
 (()=>{
-  const LOCAL_BUILD='20260916-2148';
+  const LOCAL_BUILD='20260916-2205';
   const CHECK_KEY='waytest-last-version-check';
   const CHECK_EVERY=60*1000;
   async function check(){

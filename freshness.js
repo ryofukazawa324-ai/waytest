13470bc748e16edf4cd0f7c5dc58dd5b32c74f39
ryofukazawa324-ai <ryofukazawa324@gmail.com>
2026-09-22 15:15:42 +0900

    Bump build for exam answer review

--- freshness.js
+++ freshness.js
@@ -1,5 +1,5 @@
 (()=>{
-  const LOCAL_BUILD='20260920-2003';
+  const LOCAL_BUILD='20260922-1514';
   const CHECK_KEY='waytest-last-version-check';
   const CHECK_EVERY=60*1000;
   async function check(){
